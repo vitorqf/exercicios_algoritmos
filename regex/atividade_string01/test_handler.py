a29acbb67486dfa0baa9be5de575f36f0f633b3e
Vitor Rafael <rqfvitor@gmail.com>
2022-12-12 17:59:12 -0300

    reparado comentarios da atividade01

--- regex/atividade_string01/test_handler.py
+++ regex/atividade_string01/test_handler.py
@@ -5,22 +5,18 @@ from handler import string_handle
 class TestHandler(unittest.TestCase):
     def test_string_handle(self):
 
-        # Se for inserido uma string suja mas com números l, o ou O, ela deve remover todos caracteres não números e retornar um inteiro menor que 32 bits
-
-        # Se for inserido uma string vazia ou só dado um enter, deve ser retornado um ValueError e impresso 'error'
-
-        # Se for inserido uma string que ao ser processada não gere nada, deve ser retornado um ValueError e impresso 'error'
-
-        # Se for inserido uma string que ao ser processada gere um número maior que 32 bits, deve ser retornado um ValueError e impresso 'error'
-
+        # Se for inserido uma string suja mas com letras l, o ou O, ela deve remover todos caracteres não números e retornar um inteiro menor que 32 bits
         self.assertEqual(string_handle('loO6,   67;  DADSAD, 13. 4'), 100667134)
 
+        # Se for inserido uma string vazia ou só dado um enter, deve ser retornado um ValueError e impresso 'error'
         with self.assertRaises(ValueError):
             string_handle('\n')
 
+        # Se for inserido uma string que ao ser processada não gere nada, deve ser retornado um ValueError e impresso 'error'
         with self.assertRaises(ValueError):
             string_handle('hi')
 
+        # Se for inserido uma string que ao ser processada gere um número maior que 32 bits, deve ser retornado um ValueError e impresso 'error'
         with self.assertRaises(ValueError):
             string_handle('2200000000')
 
